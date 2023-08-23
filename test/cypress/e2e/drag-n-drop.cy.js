bb769fc26687490d996fc894c44b6fd30d1d8da0
Miloš Jovanović <milos.jov.rs@gmail.com>
2023-08-24 01:59:26 +0200

    Expand drag-n-drop spec

--- test/cypress/e2e/drag-n-drop.cy.js
+++ test/cypress/e2e/drag-n-drop.cy.js
@@ -1,26 +1,53 @@
-import { el } from "@cy-support/elements"
-
 describe("Tests drag and drop functionality", { tags: "@remote" }, () => {
-  beforeEach(() => {
-    cy.visit("/drag_and_drop")
-  })
-
   it("uses HTMLElement events", () => {
+    cy.visit("/drag_and_drop")
+
     cy.get("#column-a").should("have.text", "A").and("have.attr", "draggable", "true").trigger("dragstart")
     cy.get("#column-b").trigger("drop")
     cy.get("#column-a").trigger("dragend").should("have.text", "B").and("have.attr", "style", "opacity: 1;")
   })
 
-  it.skip("uses mouse events", () => {
-    cy.get("#column-a").should("have.text", "A").and("have.attr", "draggable", "true")
-    cy.get("#column-b").then((columnB) => {
-      const bPosition = columnB.position()
-      cy.get("#column-a")
+  function getCoords(element, document) {
+    let rect = element.getBoundingClientRect()
+
+    return {
+      left: rect.left + rect.width / 2 + document.documentElement.scrollLeft,
+      top: rect.top + rect.height / 2 + document.documentElement.scrollTop,
+    }
+  }
+
+  it("uses mouse events on a simple target", () => {
+    cy.visit("https://demoqa.com/droppable")
+
+    cy.document().then((doc) => {
+      let elem = doc.querySelector("#droppable")
+      let pos = getCoords(elem, doc)
+      cy.get("#draggable")
         .trigger("mousedown", { which: 1 })
-        // .trigger("mousemove", { pageX: bPosition.top, pageY: bPosition.left })
-        // .trigger("mousemove", { clientX: 20, clientY: 20 })
-        .trigger("mousemove", { offsetX: bPosition.top, offsetY: bPosition.left })
+        .trigger("mousemove", { pageX: pos.left, pageY: pos.top })
         .trigger("mouseup")
+
+      cy.get("#droppable").should("have.text", "Dropped!")
+    })
+  })
+
+  it("uses mouse events on a complex target", () => {
+    cy.visit("https://demoqa.com/droppable")
+
+    cy.get("#droppableExample-tab-preventPropogation").click()
+    cy.document().then((doc) => {
+      let elem = doc.querySelector("#greedyDropBox>p")
+      let pos = getCoords(elem, doc)
+
+      cy.get("#dragBox")
+        .trigger("mousedown", { which: 1 })
+        .trigger("mousemove", { pageX: pos.left, pageY: pos.top, force: true })
+        .trigger("mouseup", { force: true })
+
+      cy.get("#greedyDropBox>p").should("have.text", "Dropped!")
+      cy.get("#greedyDropBoxInner").should("not.have.text", "Dropped!")
+      cy.get("#notGreedyDropBox").should("not.have.text", "Dropped!")
+      cy.get("#notGreedyInnerDropBox").should("not.have.text", "Dropped!")
     })
   })
 })
