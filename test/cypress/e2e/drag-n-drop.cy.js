6e7498f24ead83715223fa5f43d88d95e788486a
Miloš Jovanović <milos.jov.rs@gmail.com>
2023-08-23 23:30:34 +0200

    Cleanup

--- test/cypress/e2e/drag-n-drop.cy.js
+++ test/cypress/e2e/drag-n-drop.cy.js
@@ -1,5 +1,3 @@
-import { el } from "@cy-support/elements"
-
 describe("Tests drag and drop functionality", { tags: "@remote" }, () => {
   beforeEach(() => {
     cy.visit("/drag_and_drop")
@@ -9,18 +7,5 @@ describe("Tests drag and drop functionality", { tags: "@remote" }, () => {
     cy.get("#column-a").should("have.text", "A").and("have.attr", "draggable", "true").trigger("dragstart")
     cy.get("#column-b").trigger("drop")
     cy.get("#column-a").trigger("dragend").should("have.text", "B").and("have.attr", "style", "opacity: 1;")
-  })
-
-  it.skip("uses mouse events", () => {
-    cy.get("#column-a").should("have.text", "A").and("have.attr", "draggable", "true")
-    cy.get("#column-b").then((columnB) => {
-      const bPosition = columnB.position()
-      cy.get("#column-a")
-        .trigger("mousedown", { which: 1 })
-        // .trigger("mousemove", { pageX: bPosition.top, pageY: bPosition.left })
-        // .trigger("mousemove", { clientX: 20, clientY: 20 })
-        .trigger("mousemove", { offsetX: bPosition.top, offsetY: bPosition.left })
-        .trigger("mouseup")
-    })
   })
 })
