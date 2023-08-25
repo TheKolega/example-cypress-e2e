f503b1d907ac8b404eb02d1775d78649937415ec
Miloš Jovanović <milos.jov.rs@gmail.com>
2023-08-25 02:30:15 +0200

    chore: Cleanup redundant test as cy.session is now used in user-flow

--- test/cypress/e2e/remote-separate-login.cy.js
+++
@@ -1,7 +0,0 @@
-describe("Tests external website isolated login flow", { tags: "@remote" }, () => {
-  it("verifies session saving works across specs", () => {
-    cy.loginFormCookie(Cypress.env("cookieLoginUrl"))
-    cy.visit(Cypress.env("cookieVerifyUrl"))
-    cy.contains("admin status: true").should("be.visible")
-  })
-})
